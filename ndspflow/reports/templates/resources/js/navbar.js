cf5ec00ee4959cc90a5dd978bbc0fdc70d0571fc
ryanhammonds <ryan.hammonds@utexas.edu>
2020-12-30 12:05:10 -0800

    js and css reorg

---
+++ ndspflow/reports/templates/resources/js/navbar.js
@@ -0,0 +1,46 @@
+function makeNavbar(){
+  const navbar = document.getElementById("navbar");
+  const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+  function openMobileNavbar() {
+    navbar.classList.add("opened");
+    navbarToggle.setAttribute("aria-label", "Close navigation menu.");
+  }
+
+  function closeMobileNavbar() {
+    navbar.classList.remove("opened");
+    navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+  }
+
+  navbarToggle.addEventListener("click", () => {
+    if (navbar.classList.contains("opened")) {
+      closeMobileNavbar();
+    } else {
+      openMobileNavbar();
+    }
+  });
+
+  const navbarMenu = navbar.querySelector(".navbar-menu");
+  const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+  navbarLinksContainer.addEventListener("click", (clickEvent) => {
+    clickEvent.stopPropagation();
+  });
+
+  navbarMenu.addEventListener("click", closeMobileNavbar);
+
+  var coll = document.getElementsByClassName("collapsible");
+  var i;
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  }
+}
+
