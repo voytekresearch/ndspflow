7effa4fa97fad1cb44e7c182e7cdd1882f5533d5
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-06-23 17:46:38 -0700

    update array handling

--- ndspflow/workflows/workflow.py
+++ ndspflow/workflows/workflow.py
@@ -2,6 +2,8 @@
 
 from multiprocessing import Pool, cpu_count
 import matplotlib.pyplot as plt
+from functools import partial
+
 import numpy as np
 import networkx as nx
 
@@ -25,14 +27,14 @@ class WorkFlow(Simulate, Transform):
         Contains order of operations as:
         [[node_type, function, *args, **kwargs], ...]
     """
-    def __init__(self, y_arr=None, x_arr=None, **kwargs):
+    def __init__(self, y_array=None, x_array=None, **kwargs):
         """Initalize object.
 
         Parameters
         ----------
-        y_arr : ndarray, optional, default: None
+        y_array : ndarray, optional, default: None
             Y-axis values. Usually voltage or power.
-        x_arr : 1d array, optional, default: None
+        x_array : 1d array, optional, default: None
             X-axis values. Usually time or frequency.
         **kwargs
             Additional keyword arguments that sub-classes
@@ -50,11 +52,11 @@ class WorkFlow(Simulate, Transform):
 
         self.graph = None
 
-        self.y_arr = y_arr
-        self.x_arr = x_arr
+        self.y_array = y_array
+        self.x_array = x_array
 
-        self.y_arr_stash = None
-        self.x_arr_stash = None
+        self.y_array_stash = None
+        self.x_array_stash = None
         self.fork_inds = []
 
         self.results = None
@@ -82,52 +84,75 @@ class WorkFlow(Simulate, Transform):
             self.results = None
 
         if self.fork_inds is not None:
-            self.y_arr_stash = [None] * len(self.fork_inds)
-            self.x_arr_stash = [None] * len(self.fork_inds)
+            self.y_array_stash = [None] * len(self.fork_inds)
+            self.x_array_stash = [None] * len(self.fork_inds)
 
         self.return_attrs = return_attrs
 
-        if self.seeds is None:
-            # FIX this for non-seeded simulations
-            pass
+        # Infer input array type
+        if self.y_array is not None:
+            # Drop instance arrays to prevent passing copies to mp pools
+            y_array = self.y_array
+            self.y_array = None
+            x_array = self.x_array
+            self.x_array = None
+            # Track original shape
+            shape = y_array.shape[-1]
+            # Ensure 2d
+            y_array = y_array.reshape(-1, shape[-1])
+        elif self.seeds is not None:
+            # Simulation workflow
+            y_array = self.seeds
+            x_array = None
         else:
-            n_jobs = cpu_count() if n_jobs == -1 else n_jobs
-            with Pool(processes=n_jobs) as pool:
-                mapping = pool.imap(self._run, self.seeds)
+            raise ValueError('Undefined input.')
 
-                if progress is not None:
-                    _results = list(progress(mapping, total=len(self.seeds),
-                                             desc='Running Workflow'))
-                else:
-                    _results = list(mapping)
+        n_jobs = cpu_count() if n_jobs == -1 else n_jobs
 
-            if self.results is not None:
-                self.results.append(_results)
+        with Pool(processes=n_jobs) as pool:
+
+            mapping = pool.imap(partial(self._run, x_array=x_array), y_array)
+
+            if progress is not None:
+                _results = list(progress(mapping, total=len(self.seeds),
+                                         desc='Running Workflow'))
             else:
-                self.results = _results
+                _results = list(mapping)
+
+        if self.results is not None:
+            self.results.append(_results)
+        else:
+            self.results = _results
 
         # Reset temporary attributes
         self.model = None
         self.node = None
 
-    def _run(self, seed):
+    def _run(self, input, x_array=None):
         """Sub-function to allow imap parallelziation.
 
         Parameters
         ----------
-        seed : int
-            Random seed to set.
+        input : int or 1d array
+            Random seed to set if int.
+            Signal to process if 1d array.
         """
-        np.random.seed(seed)
+        # Reset de-instanced arrays
+        self.x_array = x_array
 
-        # Clear
+        if isinstance(input, int):
+            np.random.seed(input)
+        elif isinstance(input, np.ndarray):
+            self.y_array = input
+
+        # Run nodes
         for node in self.nodes:
             self.node = node
             if node[0] in ['simulate', 'transform']:
                 getattr(self, 'run_' + node[0])(node[1], *node[2],
                                                 **node[3], **node[4])
             elif node[0] == 'fit':
-                getattr(self, 'run_' + node[0])(self.x_arr, self.y_arr,
+                getattr(self, 'run_' + node[0])(self.x_array, self.y_array,
                                                 *node[2], **node[3])
             elif node[0] == 'fork':
                 getattr(self, 'run_' + node[0])(node[1])
@@ -173,15 +198,15 @@ class WorkFlow(Simulate, Transform):
             Reference to fork to rewind to.
         """
 
-        if self.y_arr_stash[ind] is None:
+        if self.y_array_stash[ind] is None:
             # Stash
-            self.y_arr_stash[ind] = self.y_arr.copy()
-            if self.x_arr is not None:
-                self.x_arr_stash[ind] = self.x_arr.copy()
-        elif self.y_arr_stash[ind] is not None:
+            self.y_array_stash[ind] = self.y_array.copy()
+            if self.x_array is not None:
+                self.x_array_stash[ind] = self.x_array.copy()
+        elif self.y_array_stash[ind] is not None:
             # Pop
-            self.y_arr = self.y_arr_stash[ind]
-            self.x_arr = self.x_arr_stash[ind]
+            self.y_array = self.y_array_stash[ind]
+            self.x_array = self.x_array_stash[ind]
 
 
     def plot(self, npad=2, ax=None, draw_kwargs=None):
