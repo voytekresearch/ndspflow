84eb1749b3227b061d339dbc266c40b023021fbf
ryanhammonds <Ryan.hammonds@utexas.edu>
2022-07-05 21:23:56 -0700

    add sim tests

--- ndspflow/workflows/sim.py
+++ ndspflow/workflows/sim.py
@@ -1,7 +1,7 @@
 """Simulations."""
 
-import types
 import operator as op
+from .utils import parse_args
 
 
 class Simulate:
@@ -29,6 +29,7 @@ class Simulate:
         self.seeds = seeds
         self.y_array = None
         self.nodes = []
+        self.param_ind = 0
 
 
     def simulate(self, func, *args, operator='add', **kwargs):
@@ -48,22 +49,7 @@ class Simulate:
 
         # Evaluate funcs/gens prior to entering mp pool
         if self.seeds is not None and not isinstance(self.seeds, int):
-
-            args = list(args)
-            for ind in range(len(args)):
-                # Get arg value if function or generator
-                if isinstance(args[ind], (types.FunctionType)):
-                    args[ind] = ('_iter', [args[ind]() for seed in self.seeds])
-                elif isinstance(args[ind], (types.GeneratorType)) or hasattr(args[ind], '__next__'):
-                    args[ind] = ('_iter', [next(args[ind])for seed in self.seeds])
-
-
-            for k, v in kwargs.items():
-                # Get kwarg value if function or generator
-                if isinstance(v, types.FunctionType):
-                    kwargs[k] = ('_iter', [v() for _ in self.seeds])
-                elif isinstance(v, types.GeneratorType) or hasattr(v, '__next__'):
-                    args[ind] = ('_iter', [next(v) for _ in self.seeds])
+            args, kwargs = parse_args(list(args), kwargs, self)
 
         self.nodes.append(['simulate', func, args,
                            {'operator': operator}, kwargs])
@@ -101,14 +87,13 @@ class Simulate:
             if isinstance(v, str) and 'self' in v:
                 kwargs[k] = getattr(self, v.split('.')[-1])
             if isinstance(v, tuple) and v[0] == '_iter':
-                kwargs[k] = v[self._pind]
+                kwargs[k] = v[1][self.param_ind]
 
         for ind in range(len(args)):
             if isinstance(args[ind], str) and 'self' in args[ind]:
                 args[ind] = getattr(self, args[ind].split('.')[-1])
             if isinstance(args[ind], tuple) and args[ind][0] == '_iter':
-                args[ind] = args[ind][1][self._pind]
-
+                args[ind] = args[ind][1][self.param_ind]
 
         # Simulate
         if self.y_array is None:
